37d5ecfa3ee9a1b0aeb3d63ce82c13dbe7ee2366
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-10 07:39:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var theBeatlesPlay= (musicians,instruments) =>{
   var array=[];
-  for(let i=0;i<musicians.length();i+=1){
+  for(let i=0;i<musicians.length;i+=1){
     array.push(`$[musicians[i]] plays $[instrument[i]]`)
   }
   return array;
