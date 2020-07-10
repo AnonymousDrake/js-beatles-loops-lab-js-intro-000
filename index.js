079694bdbf388386a7983f26d024bd025994bd27
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-10 07:40:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var theBeatlesPlay= (musicians,instruments) =>{
   var array=[];
   for(let i=0;i<musicians.length;i+=1){
-    array.push($[musicians[i]]+" plays " +$[instrument[i]])
+    array.push(musicians[i]+" plays " +instrument[i])
   }
   return array;
 }
